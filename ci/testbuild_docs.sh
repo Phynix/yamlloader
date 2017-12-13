8756676dbd8bf658d31dcaa5f9eb54374471205c
Jonas 'Mayou36' Eschle <mayou36@jonas.eschle.com>
2017-12-13 20:30:58 +0100

    Fix missing docs not created

--- ci/testbuild_docs.sh
+++ ci/testbuild_docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #    test build docs
 echo "============================ Building docs for test ============================"
-conda install sphinx sphinx_rtd_theme > tmp.txt && echo 'test utils installed'
+conda install sphinx sphinx_rtd_theme > tmp.txt && echo 'doc utils installed'
 bash docs/make_docs.sh 2>&1 | tail -n 11
 echo "======================= Finished building docs for test ========================"
