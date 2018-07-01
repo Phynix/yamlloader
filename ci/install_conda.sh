13e6296643ac8d8bca096507e908fa97184bc3c9
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-07-01 12:06:19 +0200

    Fix BASH typo

--- ci/install_conda.sh
+++ ci/install_conda.sh
@@ -8,7 +8,7 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O anaconda.sh;
 fi
 
-if [[ "$PHYNIX_PYTHON_VERSION"="3.7-dev" ]]; then
+if [[ "$PHYNIX_PYTHON_VERSION" == "3.7-dev" ]]; then
         PHYNIX_PYTHON_VERSION="3.7";
 fi
 
