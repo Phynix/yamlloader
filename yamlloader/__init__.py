fc4f908b6533e22f30ba4b08f74c4922e2ca307b
Jonas Eschle <mayou36@jonas.eschle.com>
2022-12-02 17:45:56 +0100

    release:  1.2.1 add Python 3.11 support

--- yamlloader/__init__.py
+++ yamlloader/__init__.py
@@ -10,7 +10,7 @@ if sys.version_info[:2] < (2, 7):
         "Please upgrade your distribution and/or packages."
     )
 
-__version__ = "1.1.0"
+__version__ = "1.2.1"
 __author__ = 'Jonas Eschle "Mayou36", Johannes Lade "SebastianJL"'
 __email__ = "jonas.eschle@phynix.science, johannes.lade@phynix.science"
 
